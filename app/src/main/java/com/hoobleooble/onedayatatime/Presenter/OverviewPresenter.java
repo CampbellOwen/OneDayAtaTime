967c2e84607c0e35a5031e747d2346379a9a1dfc
Owen Campbell <ocampbel@sfu.ca>
2017-09-17 08:31:28 -0700

    want 2 die

---
+++ app/src/main/java/com/hoobleooble/onedayatatime/Presenter/OverviewPresenter.java
@@ -0,0 +1,12 @@
+package com.hoobleooble.onedayatatime.Presenter;
+
+import android.content.Context;
+
+/**
+ * Created by Owen Campbell on 9/17/2017.
+ */
+
+public class OverviewPresenter {
+
+    private Context viewContext;
+}

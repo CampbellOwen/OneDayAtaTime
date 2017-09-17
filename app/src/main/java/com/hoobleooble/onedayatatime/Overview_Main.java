c473d8867f26c33abca208553d840ff06db0ef85
HeatherRobyn <hmaskell@sfu.ca>
2017-09-17 09:57:12 -0700

    Rating converter added + some style shtuff and a botton on the overview page

--- app/src/main/java/com/hoobleooble/onedayatatime/Overview_Main.java
+++ app/src/main/java/com/hoobleooble/onedayatatime/Overview_Main.java
@@ -30,60 +30,16 @@ public class Overview_Main extends AppCompatActivity {
 
     public static void main(String args[]) {
 
-        HashMap<Integer, String> rateDay = new HashMap<Integer, String>();
-
-        rateDay.put(1, "a very bad day");
-        rateDay.put(2, "a bad day");
-        rateDay.put(3, "a fairly bad day");
-        rateDay.put(4, "a moderately bad day");
-        rateDay.put(5, "an average day");
-        rateDay.put(6, "a moderately good day");
-        rateDay.put(7, "a fairly good day");
-        rateDay.put(8, "a good day");
-        rateDay.put(9, "a great day");
-        rateDay.put(10, "an awesome day");
 
 
-        HashMap<Integer, String> rateInteraction = new HashMap<Integer, String>();
-
-        rateInteraction.put(1, "truly horrible");
-        rateInteraction.put(2, "very unenjoyable");
-        rateInteraction.put(3, "unenjoyable");
-        rateInteraction.put(4, "somewhat unenjoyable");
-        rateInteraction.put(5, "average");
-        rateInteraction.put(6, "somewhat enjoyable");
-        rateInteraction.put(7, "enjoyable");
-        rateInteraction.put(8, "very enjoyable");
-        rateInteraction.put(9, "thoroughly enjoyable");
-        rateInteraction.put(10, "incredibly enjoyable");
 
 
-        HashMap<Integer, String> rateSleep = new HashMap<Integer, String>();
-
-        rateSleep.put(1, "nowhere near enough sleep");
-        rateSleep.put(2, "barely any sleep");
-        rateSleep.put(3, "very little sleep");
-        rateSleep.put(4, "little sleep");
-        rateSleep.put(5, "fairly little sleep");
-        rateSleep.put(6, "not quite enough sleep");
-        rateSleep.put(7, "almost enough sleep");
-        rateSleep.put(8, "a full 8 hours of sleep");
-        rateSleep.put(9, "more than enough sleep");
-        rateSleep.put(10, "more than enough sleep");
 
 
-        HashMap<Integer, String> rateMeal = new HashMap<Integer, String>();
 
-        rateMeal.put(1, "very unhealthy");
-        rateMeal.put(2, "unhealthily");
-        rateMeal.put(3, "fairly unhealthily");
-        rateMeal.put(4, "moderately unhealthily");
-        rateMeal.put(5, "neither healthily nor unhealthily");
-        rateMeal.put(6, "moderately healthily");
-        rateMeal.put(7, "fairly healthily");
-        rateMeal.put(8, "fairly healthily");
-        rateMeal.put(9, "healthily");
-        rateMeal.put(10, "very healthily");
+
+
+
 
     }
 
---
+++ app/src/main/java/com/hoobleooble/onedayatatime/Utilities/RatingConverter.java
@@ -0,0 +1,128 @@
+package com.hoobleooble.onedayatatime.Utilities;
+
+import java.util.HashMap;
+
+/**
+ * Created by HeatherRobyn on 2017-09-17.
+ */
+
+public class RatingConverter {
+
+    public enum ratingType{
+        sleep,
+        interaction,
+        meal,
+        day
+    }
+
+    public static String convert(int Rating, ratingType type){
+        switch (type){
+            case day:
+                switch (Rating){
+                    case 1:
+                        return "a very bad day";
+                    case 2:
+                        return "a bad day";
+                    case 3:
+                        return "a fairly bad day";
+                    case 4:
+                        return "a moderately bad day";
+                    case 5:
+                        return "an average day";
+                    case 6:
+                        return "a moderately good day";
+                    case 7:
+                        return "a fairly good day";
+                    case 8:
+                        return "a good day";
+                    case 9:
+                        return "a great day";
+                    case 10:
+                        return "an awesome day";
+                    default:
+                        return "a good day";
+                }
+
+            case meal:
+
+                switch (Rating){
+                    case 1:
+                        return "very unhealthily";
+                    case 2:
+                        return "unhealthily";
+                    case 3:
+                        return "fairly unhealthily";
+                    case 4:
+                        return "moderately unhealthily";
+                    case 5:
+                        return "neither healthily nor unhealthily";
+                    case 6:
+                        return "moderately healthily";
+                    case 7:
+                        return "fairly healthily";
+                    case 8:
+                        return "healthily";
+                    case 9:
+                        return "healthily";
+                    case 10:
+                        return "very healthily";
+                    default:
+                        return "food";
+                }
+
+            case sleep:
+
+                switch (Rating){
+                    case 1:
+                        return "nowhere near enough sleep";
+                    case 2:
+                        return "barely any sleep";
+                    case 3:
+                        return "very little sleep";
+                    case 4:
+                        return "little sleep";
+                    case 5:
+                        return "fairly little sleep";
+                    case 6:
+                        return "not quite enough sleep";
+                    case 7:
+                        return "almost enough sleep";
+                    case 8:
+                        return "a full 8 hours of sleep";
+                    case 9:
+                        return "more than enough sleep";
+                    case 10:
+                        return "more than enough sleep";
+                    default:
+                        return "some sleep";
+                }
+
+            case interaction:
+                switch (Rating){
+                    case 1:
+                        return "a truly horrible time";
+                    case 2:
+                        return "a very unenjoyable time";
+                    case 3:
+                        return "an unenjoyable time";
+                    case 4:
+                        return "a somewhat unenjoyable time";
+                    case 5:
+                        return "an average time";
+                    case 6:
+                        return "a pretty good time";
+                    case 7:
+                        return "a good time";
+                    case 8:
+                        return "a very good time";
+                    case 9:
+                        return "a great time";
+                    case 10:
+                        return "an amazing time";
+                    default:
+                        return "a good time";
+                }
+        }
+    return "";
+    }
+}

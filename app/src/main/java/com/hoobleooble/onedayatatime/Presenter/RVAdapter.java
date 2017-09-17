fff7e36a886b3267483d2c49665a3ffb5db3a8fd
Owen Campbell <ocampbel@sfu.ca>
2017-09-16 21:07:25 -0700

    Moved card to card_string.xml

--- app/src/main/java/com/hoobleooble/onedayatatime/Presenter/RVAdapter.java
+++ app/src/main/java/com/hoobleooble/onedayatatime/Presenter/RVAdapter.java
@@ -2,6 +2,8 @@ package com.hoobleooble.onedayatatime.Presenter;
 
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -11,14 +13,23 @@ import android.widget.TextView;
 import com.hoobleooble.onedayatatime.Model.Question;
 import com.hoobleooble.onedayatatime.R;
 
+import java.util.List;
+
 /**
  * Created by Owen Campbell on 9/16/2017.
  */
 
 public class RVAdapter extends RecyclerView.Adapter<RVAdapter.QuestionViewHolder>
 {
+    List<Question> questions;
+    public RVAdapter(List<Question> questions)
+    {
+        this.questions = questions;
+    }
+
     @Override
     public QuestionViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout)
         return null;
     }
 
@@ -29,7 +40,7 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.QuestionViewHolder
 
     @Override
     public int getItemCount() {
-        return 0;
+        return questions.size();
     }
 
     public static class QuestionViewHolder extends RecyclerView.ViewHolder
@@ -45,6 +56,9 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.QuestionViewHolder
             super(itemView);
             cv = (CardView)itemView.findViewById(R.id.cv);
             question = (TextView)itemView.findViewById(R.id.question);
+            subText = (TextView)itemView.findViewById(R.id.subtext);
+            answer = (EditText)itemView.findViewById(R.id.answer);
+            button = (Button)itemView.findViewById(R.id.nextButton);
         }
     }
 }

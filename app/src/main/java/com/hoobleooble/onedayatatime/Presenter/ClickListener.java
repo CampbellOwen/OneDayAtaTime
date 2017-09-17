0795c03278b4c4555ef5de86bebf9290c2b5198d
Owen Campbell <ocampbel@sfu.ca>
2017-09-17 02:14:59 -0700

    ahhhhhh

---
+++ app/src/main/java/com/hoobleooble/onedayatatime/Presenter/ClickListener.java
@@ -0,0 +1,12 @@
+package com.hoobleooble.onedayatatime.Presenter;
+
+import android.view.View;
+
+/**
+ * Created by Owen Campbell on 9/17/2017.
+ */
+
+public interface ClickListener {
+
+    void onPositionClicked(View v, int position);
+}
--- app/src/main/java/com/hoobleooble/onedayatatime/Presenter/QuestionsPresenter.java
+++ app/src/main/java/com/hoobleooble/onedayatatime/Presenter/QuestionsPresenter.java
@@ -1,8 +1,56 @@
 package com.hoobleooble.onedayatatime.Presenter;
 
+import android.app.Presentation;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.hoobleooble.onedayatatime.Model.Answer;
+import com.hoobleooble.onedayatatime.Model.DbHelper;
+import com.hoobleooble.onedayatatime.Model.Question;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
 /**
  * Created by Owen Campbell on 9/16/2017.
  */
 
 public class QuestionsPresenter {
+
+    private List<Question> questions;
+    private DbHelper dbHelper;
+    private SQLiteDatabase db;
+
+    public QuestionsPresenter(Context context)
+    {
+        questions = new ArrayList<Question>();
+        dbHelper = new DbHelper(context);
+        db = dbHelper.getWritableDatabase();
+        for(int i = 1; i <= 20; i++){
+            questions.add(new Question(db, i));
+        }
+
+    }
+
+    public List<Question> getQuestions()
+    {
+        return this.questions;
+    }
+
+    public boolean storeAnswer(String answer, int qid)
+    {
+        try{
+            int date = (int)Calendar.getInstance().getTime().getTime();
+            Answer.Create(db, qid, date, answer);
+            Log.d("DB", "Successfully saved answer: qid: " + Integer.toString(qid) + ", date: " + Integer.toString(date) + ", answer: " + answer);
+            return true;
+        }
+        catch (Exception e){
+            Log.d("DB", "Saving answer failed :(");
+            return false;
+        }
+    }
+
 }
